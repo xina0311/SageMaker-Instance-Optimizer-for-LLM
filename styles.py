fe5eae2a792932bc3c4ed1e575a2376691c6d477
Amy Li <amy20080311@gmail.com>
2024-12-30 15:43:40 +0800

    Remove gray background from calc-details section

--- styles.py
+++ styles.py
@@ -1,6 +1,19 @@
 def get_styles():
     return """
 <style>
+    .main .block-container {
+        padding-top: 1rem;
+        padding-bottom: 0rem;
+        padding-left: 5rem;
+        padding-right: 5rem;
+    }
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 400px;
+        margin-left: -400px;
+    }
     .main-title {
         font-size: 24px;
         font-weight: bold;
@@ -15,8 +28,11 @@ def get_styles():
         border-radius: 5px;
         margin-bottom: 20px;
     }
-    .calc-details, .suitable-instances {
-        background-color: #F0F0F0;
+    .calc-details {
+        margin-bottom: 20px;
+    }
+    .suitable-instances {
+        background-color: #E6F2FF;
         padding: 10px;
         border-radius: 5px;
         margin-bottom: 20px;
